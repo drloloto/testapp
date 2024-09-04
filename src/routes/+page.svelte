99c6e17a697147e8c3e6eb0fb87fb4cfea40dbda
drloloto <sebastian.hess@pm.me>
2024-09-04 07:03:58 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import "atomico/ssr/load"
+    import Milkdown from './Milkdown.svelte';
+</script>
+
+<Milkdown/>
---
+++ src/routes/Milkdown.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { Editor, defaultValueCtx, rootCtx } from "@milkdown/kit/core";
+	import { imageBlockComponent } from "@milkdown/kit/component/image-block";
+	import { commonmark } from "@milkdown/kit/preset/commonmark";
+	import { nord } from "@milkdown/theme-nord";
+	import "@milkdown/theme-nord/style.css";
+
+	const markdown = `
+    # Milkdown Component Image Inline
+
+    Crepe provides three themes: Crepe ![crepe](/crepe.png), Nord ![](/nord.png) and Script ![](/script.png).
+
+    Maybe more? ![]()
+
+    This is a demo for using [Milkdown](https://milkdown.dev) image inline component
+    `;
+
+	function editor(dom: HTMLDivElement) {
+		// to obtain the editor instance we need to store a reference of the editor.
+		const MakeEditor = Editor.make()
+			.config((ctx) => {
+				ctx.set(rootCtx, dom);
+				ctx.set(defaultValueCtx, markdown);
+			})
+
+			.config(nord)
+			.use(commonmark)
+			.create();
+		MakeEditor.then((editor) => {
+			editor.action((ctx) => {});
+		});
+	}
+</script>
+<div use:editor></div>
