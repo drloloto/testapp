afa5eaf6756ebe010d87b285c741fb1ce10c2788
drloloto <sebastian.hess@pm.me>
2024-09-07 10:40:28 +0200

    document.querySelectorAll is not a function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-    import "atomico/ssr/load"
-    import "./polyfill"
-    import Milkdown from './Milkdown.svelte';
+  // not commeting out the following lines causing error "document.querySelectorAll is not a function"
+  // import "atomico/ssr/load";
+  import "./polyfill";
+  import Milkdown from "./Milkdown.svelte";
+  import * as Resizable from "$lib/components/ui/resizable";
 </script>
 
-<Milkdown/>
+<Resizable.PaneGroup direction="horizontal">
+  <Resizable.Pane class="bg-yellow-50">
+    <Milkdown />
+  </Resizable.Pane>
+  <Resizable.Handle />
+  <Resizable.Pane class="bg-red-50">Two</Resizable.Pane>
+</Resizable.PaneGroup>
--- src/routes/Milkdown.svelte
+++ src/routes/Milkdown.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	import { Editor, defaultValueCtx, rootCtx } from "@milkdown/kit/core";
-	import { imageBlockComponent } from "@milkdown/kit/component/image-block";
-	import { commonmark } from "@milkdown/kit/preset/commonmark";
-	import { nord } from "@milkdown/theme-nord";
-	import "@milkdown/theme-nord/style.css";
+  import { Editor, defaultValueCtx, rootCtx } from "@milkdown/kit/core";
+  //   import { imageBlockComponent } from "@milkdown/kit/component/image-block";
+  import { commonmark } from "@milkdown/kit/preset/commonmark";
+  import { nord } from "@milkdown/theme-nord";
 
-	const markdown = `
+  const markdown = `
     # Milkdown Component Image Inline
 
     Crepe provides three themes: Crepe ![crepe](/crepe.png), Nord ![](/nord.png) and Script ![](/script.png).
@@ -15,20 +14,19 @@
     This is a demo for using [Milkdown](https://milkdown.dev) image inline component
     `;
 
-	function editor(dom: HTMLDivElement) {
-		// to obtain the editor instance we need to store a reference of the editor.
-		const MakeEditor = Editor.make()
-			.config((ctx) => {
-				ctx.set(rootCtx, dom);
-				ctx.set(defaultValueCtx, markdown);
-			})
+  function editor(dom: HTMLDivElement) {
+    // to obtain the editor instance we need to store a reference of the editor.
+    const MakeEditor = Editor.make()
+      .config((ctx) => {
+        ctx.set(rootCtx, dom);
+        ctx.set(defaultValueCtx, markdown);
+      })
 
-			.config(nord)
-			.use(commonmark)
-			.create();
-		MakeEditor.then((editor) => {
-			editor.action((ctx) => {});
-		});
-	}
+      .config(nord)
+      .use(commonmark)
+      .create();
+    MakeEditor.then((editor) => {});
+  }
 </script>
+
 <div use:editor></div>
